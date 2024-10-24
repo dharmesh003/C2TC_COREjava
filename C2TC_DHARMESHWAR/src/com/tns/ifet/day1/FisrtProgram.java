f36155bccaf6556016ea4c88c995713f0b871d5b
Lenovo <Lenovo@LAPTOP-9GIG1T8E>
2024-10-24 18:08:37 +0530

    First Program

---
+++ C2TC_DHARMESHWAR/src/com/tns/ifet/day1/FisrtProgram.java
@@ -0,0 +1,10 @@
+package com.tns.ifet.day1;
+
+public class FisrtProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Hello World");
+	}
+
+}
