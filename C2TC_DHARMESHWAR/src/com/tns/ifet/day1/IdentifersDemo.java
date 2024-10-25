044a50a2526a040f7169054b732befb31d8bf378
Lenovo <Lenovo@LAPTOP-9GIG1T8E>
2024-10-25 14:42:29 +0530

    identifers

---
+++ C2TC_DHARMESHWAR/src/com/tns/ifet/day1/IdentifersDemo.java
@@ -0,0 +1,13 @@
+package com.tns.ifet.day1;
+
+public class IdentifersDemo {
+
+	public static void main(String[] args) {
+		int _number= 29; //starts with $ 
+		System.out.println("value of the number variable is : "+_number); 
+		String Name="Dharmeshwar"; //use camelcase 
+		System.out.println("value is : "+ Name); 
+
+	}
+
+}
