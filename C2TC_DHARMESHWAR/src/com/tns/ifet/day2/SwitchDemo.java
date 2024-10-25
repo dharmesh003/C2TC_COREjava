259b8d64504aaca49629d8b87d0c3d918bbf4a37
Lenovo <Lenovo@LAPTOP-9GIG1T8E>
2024-10-25 16:16:04 +0530

    dayon2

---
+++ C2TC_DHARMESHWAR/src/com/tns/ifet/day2/SwitchDemo.java
@@ -0,0 +1,23 @@
+package com.tns.ifet.day2;
+
+public class SwitchDemo {
+
+	public static void main(String[] args) {
+		int i=10;
+		switch (i) {
+		case 1:
+			System.out.println(1);
+			break;
+		case 10:
+			System.out.println(10);
+			break;
+		case 100:
+			System.out.println(100);
+			break;
+
+		default:
+			break;
+		}
+	}
+
+}
