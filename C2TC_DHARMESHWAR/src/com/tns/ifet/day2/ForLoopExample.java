df8b17b84a4eeb8f4515276d06ec15fff817fc61
Lenovo <Lenovo@LAPTOP-9GIG1T8E>
2024-10-25 17:12:12 +0530

    dayon2

---
+++ C2TC_DHARMESHWAR/src/com/tns/ifet/day2/ForLoopExample.java
@@ -0,0 +1,12 @@
+package com.tns.ifet.day2;
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++) {
+			System.out.println(i+"\n");
+		}
+
+	}
+
+}
