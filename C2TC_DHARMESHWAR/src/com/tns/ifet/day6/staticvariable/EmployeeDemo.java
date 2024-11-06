d93dd80053d0d96a11ba4e4fdadc7e7b78bf9fd6
Lenovo <Lenovo@LAPTOP-9GIG1T8E>
2024-11-06 12:03:37 +0530

    day6

---
+++ C2TC_DHARMESHWAR/src/com/tns/ifet/day6/staticvariable/EmployeeDemo.java
@@ -0,0 +1,15 @@
+package com.tns.ifet.day6.staticvariable;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		Employee e = new Employee("Dharmesh", 29);
+		System.out.println(e);
+		
+		
+		e = new Employee("Dark", 666);
+		System.out.println(e);
+
+	}
+
+}
---
+++ C2TC_DHARMESHWAR/src/com/tns/ifet/day6/usingfinal/FinalClass.java
@@ -0,0 +1,9 @@
+package com.tns.ifet.day6.usingfinal;
+
+public final class FinalClass {
+	void show() {
+		System.out.println("Final class cannot be inherited");
+	}
+}
+
+
