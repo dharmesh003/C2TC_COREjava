7f55dce130bc1f522a835a5be0b6c46b62f07d1c
Lenovo <Lenovo@LAPTOP-9GIG1T8E>
2024-12-14 17:58:09 +0530

    Assignment1

---
+++ C2TC_DHARMESHWAR/src/com/tns/ifet/AssignmentMain/AssignmentMain.java
@@ -0,0 +1,10 @@
+package com.tns.ifet.AssignmentMain;
+import com.tns.ifet.AssignmentMain.Manager;
+public class AssignmentMain {
+public static void main(String[] args) {
+Manager manager = new Manager("John", 101, 90000, "Sales");
+Developer developer = new Developer("Alice", 102, 80000, "Java");
+EmployeeUtilities.printEmployeeDetails(manager);
+EmployeeUtilities.printEmployeeDetails(developer);
+}
+}
